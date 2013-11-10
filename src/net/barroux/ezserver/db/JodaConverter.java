ccd753fc59b54074ce19340e39a24476e30716d3
Bertrand Barroux <bbarroux@gmail.com>
2013-11-10 23:46:29 +0100

    Added support for Joda-time in jooq

---
+++ src/net/barroux/ezserver/db/JodaConverter.java
@@ -0,0 +1,29 @@
+package net.barroux.ezserver.db;
+
+import java.sql.Timestamp;
+import org.joda.time.DateTime;
+import org.jooq.Converter;
+
+public class JodaConverter implements Converter<Timestamp, DateTime> {
+
+	@Override
+	public DateTime from(Timestamp databaseObject) {
+		return (databaseObject == null) ? null : new DateTime(databaseObject.getTime());
+	}
+
+	@Override
+	public Timestamp to(DateTime userObject) {
+		return (userObject == null) ? null : new Timestamp(userObject.getMillis());
+	}
+
+	@Override
+	public Class<Timestamp> fromType() {
+		return Timestamp.class;
+	}
+
+	@Override
+	public Class<DateTime> toType() {
+		return DateTime.class;
+	}
+
+}
